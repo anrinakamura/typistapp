ce6f2103b5b1ae0dacf53a06a305cd0269ac157d
anrinakamura <89412831+anrinakamura@users.noreply.github.com>
2025-07-11 12:41:32 +0900

    update

--- composeApp/src/commonMain/kotlin/app/typist/App.kt
+++ composeApp/src/commonMain/kotlin/app/typist/App.kt
@@ -1,20 +1,28 @@
 package app.typist
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -62,24 +70,37 @@ fun App() {
             }
         }
 
-        // TODO: move to ViewModel
-        when (val state = uiState) {
-            is AppUiState.Loading -> {
-                CircularProgressIndicator()
-            }
-            is AppUiState.Generating -> {
-                // TODO: move to ViewModel
-                val anImage = imageResource(Res.drawable.resized_monalisa)
-                val result = TypistArtConverter(state.typesetElements).convert(32, anImage)
-                uiState = AppUiState.Success(result)
-            }
-            is AppUiState.Success -> {
-                // TODO: animate the typist-art
-                // PrintTypistArt(state.typistArt)
-                AnimateTypistArt(state.typistArt.joinToString("\n"))
-            }
-            is AppUiState.Error -> {
-                Text(state.message)
+        Column(
+            modifier = Modifier.fillMaxSize().padding(vertical = 128.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            // TODO: move to ViewModel
+            when (val state = uiState) {
+                is AppUiState.Loading -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        CircularProgressIndicator(modifier = Modifier.width(64.dp))
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text("Generating typist-art...")
+                    }
+                }
+                is AppUiState.Generating -> {
+                    // TODO: move to ViewModel
+                    val anImage = imageResource(Res.drawable.resized_monalisa)
+                    val result = TypistArtConverter(state.typesetElements).convert(32, anImage)
+                    uiState = AppUiState.Success(result)
+                }
+                is AppUiState.Success -> {
+                    // TODO: animate the typist-art
+                    // PrintTypistArt(state.typistArt)
+                    AnimateTypistArt(state.typistArt.joinToString("\n"))
+                }
+                is AppUiState.Error -> {
+                    Text(state.message)
+                }
             }
         }
     }
@@ -103,7 +124,6 @@ fun AnimateTypistArt(text: String) {
 
     Text(
         text = subString,
-        modifier = Modifier,
         fontSize = 12.sp,
         fontFamily = notoSanJP, // FontFamily.Monospace,
         lineHeight = 12.sp,
@@ -115,17 +135,17 @@ fun PrintTypistArt(lines: List<String>) {
     val notoSanJP = FontFamily(Font(Res.font.NotoSansJP_Regular, FontWeight.Normal))
 
     BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
-//        val maxWidth = constraints.maxWidth // maxWidth.toDouble()
-//        val maxHeight = constraints.maxHeight // maxHeight.toDouble()
-//        val fontSizePx =
-//            minOf(
-//                // TODO: get columns from ViewModel
-//                maxWidth / 32,
-//                maxHeight / lines.size,
-//            ) * 0.99f
-//        println("maxWidth: $maxWidth, maxHeight: $maxHeight, fontSize: $fontSizePx")
-//
-//        val fontSizeSp = with(LocalDensity.current) { fontSizePx.toSp() }
+        // val maxWidth = constraints.maxWidth
+        // val maxHeight = constraints.maxHeight
+        // val fontSizePx =
+        //     minOf(
+        //         // TODO: get columns from ViewModel
+        //         maxWidth / 32,
+        //         maxHeight / lines.size,
+        //     ) * 0.99f
+        // println("maxWidth: $maxWidth, maxHeight: $maxHeight, fontSize: $fontSizePx")
+
+        // val fontSizeSp = with(LocalDensity.current) { fontSizePx.toSp() }
 
         Box(modifier = Modifier.fillMaxSize()) {
             // TODO: allow to use input image
@@ -138,25 +158,10 @@ fun PrintTypistArt(lines: List<String>) {
 
             BasicText(
                 text = lines.joinToString("\n"),
-//                modifier = Modifier.fillMaxWidth(),
                 style = TextStyle(fontFamily = notoSanJP),
                 maxLines = 1,
                 autoSize = TextAutoSize.StepBased(),
             )
-
-//            Column(modifier = Modifier.fillMaxWidth().padding(0.dp)) {
-//                lines.forEach { line ->
-//                    BasicText(
-//                        text = line,
-//                         modifier = Modifier.fillMaxWidth(),
-//                        style = TextStyle(
-//                            fontFamily = FontFamily.Monospace,
-//                        ),
-//                        maxLines = 1,
-//                        autoSize = TextAutoSize.StepBased(),
-//                    )
-//                }
-//            }
         }
     }
 }
